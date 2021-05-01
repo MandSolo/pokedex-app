2770b869a8ea0fb576a5cf9d9d675592769c921c
Mand Cashin <mandcashin@hotmail.co.uk>
2021-05-01 13:03:25 +0100

    wip: setting up graphql apolloprovider

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import React from "react";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "@apollo/react-hooks";
+
 function App() {
-  return <div className="App">app</div>;
+  const client = new ApolloClient({
+    uri: "https://graphql-pokemon.sh",
+  });
+
+  return (
+    <ApolloProvider client={client}>
+      <main>
+        <p>pokemon graphql stuff</p>
+      </main>
+    </ApolloProvider>
+  );
 }
 
 export default App;
